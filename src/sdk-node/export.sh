a2e1a902b7579982963923e8e21819ce31b77a1b
LuatCoi97 <mabongdem123@gmail.com>
2018-12-25 04:50:42 +0700

    update new status

--- src/sdk-node/export.sh
+++ src/sdk-node/export.sh
@@ -7,3 +7,13 @@ export PEER_DOMAIN=peer0.org1;
 export CA_SERVER_NAME=ca;
 export MSPID=Org1MSP;
 export TLS_ENABLED=true;
+
+export CA_HOST=localhost:30500;
+export PEER_HOST=localhost:30501;
+export EVENT_HOST=localhost:30603;
+export ORDERER_HOST=localhost:32001;
+export ORDERER_DOMAIN=orderer1.orgorderer;
+export PEER_DOMAIN=peer0.org2;
+export CA_SERVER_NAME=ca;
+export MSPID=Org1MSP;
+export TLS_ENABLED=true;
